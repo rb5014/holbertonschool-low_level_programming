b5e1e699e6ff6f12421fd8794240722735c868ee
rb5014 <5014@holbertonschool.com>
2022-06-08 21:49:56 +0200

    7-print_last_digit.c: supression de n = n % 10, ajout de l'opération directement dans _putchar et return

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -6,14 +6,10 @@
  * @n: the parameter to be computed to get the last digit
  * Return: n (the value of the last digit)
  */
-
 int print_last_digit(int n)
 {
-	int a;
-
 	if (n < 0)
 		n = n * -1;
-	a = n % 10;
-	_putchar(a + '0');
-	return (a);
+	_putchar((n % 10) + '0');
+	return (n % 10);
 }
