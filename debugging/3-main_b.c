8d2fa910d4ac4b1a262dea4e9e4fd3723816c150
rb5014 <5014@holbertonschool.com>
2022-06-10 13:47:17 +0200

    3-print_remaining_days.c fixed

--- debugging/3-main_b.c
+++ debugging/3-main_b.c
@@ -13,8 +13,8 @@ int main(void)
     int day;
     int year;
 
-    month = 3;
-    day = 1;
+    month = 2;
+    day = 29;
     year = 2008;
 
     printf("Date: %02d/%02d/%04d\n", month, day, year);
--- debugging/3-print_remaining_days.c
+++ debugging/3-print_remaining_days.c
@@ -14,13 +14,14 @@ void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
 	{
-		if (month >= 2 && day > 59)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
+	/*
 	else
 	{
 		if (month == 2 && day == 60)
@@ -34,4 +35,5 @@ void print_remaining_days(int month, int day, int year)
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
+	*/
 }
