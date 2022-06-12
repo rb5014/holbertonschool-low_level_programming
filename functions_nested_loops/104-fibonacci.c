0513d2da6022846a9ea2c13ea740132752b06197
rb5014 <5014@holbertonschool.com>
2022-06-12 14:53:20 +0200

    , added on first time

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -9,23 +9,37 @@
 
 int main(void)
 {
-	long double f1 = 1, f2 = 2, f3;
-	int firsttime = 1, i;
+	unsigned long int f1 = 1, f1a, f1b, f2 = 2, f2a, f2b, f3, f3a, f3b;
+	int i;
 
-	for (i = 0; i <= 96; i++)
+	for (i = 0; i <= 97; i++)
 	{
-		if (firsttime == 1)
+		if (i == 0)
 		{
-			printf("%.0Lf %.0Lf", f1, f2);
-			firsttime = 0;
+			printf("%lu, %lu", f1, f2);
+		}
+		else if (i <= 91)
+		{
+			f1a = f1 / 100000;
+			f1b = f1 % 100000;
+			f2a = f2 / 100000;
+			f2b = f2 % 100000;
+			printf(", ");
+			f3 = f1 + f2;
+			printf("%lu", f3);
+			f1 = f2;
+			f2 = f3;
 		}
 		else
 		{
 			printf(", ");
-			f3 = f1 + f2;
-			printf("%.0Lf", f3);
-			f1 = f2;
-			f2 = f3;
+			f3a = (f1a + f2a);
+			f3b = (f1b + f2b);
+			printf("%lu%lu", f3a, f3b);
+			f1a = f2a;
+			f1b = f2b;
+			f2a = f3a;
+			f2b = f3b;
 		}
 	}
 	printf("\n");
