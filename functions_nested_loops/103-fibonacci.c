391fce2e698b773eaa04113ef58ba1f9c5c9145e
rb5014 <5014@holbertonschool.com>
2022-06-12 12:31:20 +0200

    103-fibonacci.c new code, just check if number is even

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
- * main - prints 50 1st Fibonacci numbers, start 1 and 2, +\n
+ * main - prints the sum of fibonacci even numbers under 4000000
  *
  * Return: 0 (always success)
  */
@@ -9,18 +9,16 @@
 
 int main(void)
 {
-	unsigned long f1 = 1, f2 = 2, f3 = 0, even = 0;
-	int i;
+	unsigned long f1 = 0, f2 = 1, f3 = 0, sumeven = 0;
 
-	while (f1 <= 4000000 && f2 <= 4000000)
+	while (f3 <= 4000000)
 	{
+		if (f2 % 2 == 0)
+			sumeven += f2;
 		f3 = f1 + f2;
 		f1 = f2;
 		f2 = f3;
-		if (i % 3 == 0)
-			even = even + f3;
-		i++;
 	}
-	printf("%lu\n", even);
+	printf("%lu\n", sumeven);
 	return (0);
 }
