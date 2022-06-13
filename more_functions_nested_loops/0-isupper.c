c49bb442b5a8c6aebb0576e78552bebe7102719f
rb5014 <5014@holbertonschool.com>
2022-06-13 10:05:13 +0200

    betty for 0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,14 @@
 /**
  * _isupper - checks for uppercase character
  *
- * @c - character to check
+ * @c: character to check
  *
  * Return: 1 if c i suppercase, 0 otherwise
  */
 
 int _isupper(int c)
 {
-        if (c >= 65 && c <= 90)
-                return (1);
-        return (0);
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
 }
