435d621799e2f03bcde9d7d756edd7efbb377604
rb5014 <5014@holbertonschool.com>
2022-06-13 12:43:18 +0200

    7-print_diagonal.c fixed

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -13,13 +13,20 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		for (j = 1; j <= i; j++)
+		for (i = 0; i < n; i++)
 		{
-			_putchar(32);
+			j = 0;
+			while (j < i)
+			{
+				_putchar(32);
+				j++;
+			}
+			_putchar('\\');
+			_putchar('\n');
 		}
-		_putchar('\\');
-		_putchar('\n');
 	}
+	else
+		_putchar('\n');
 }
