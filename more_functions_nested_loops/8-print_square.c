0d6986ca559b0a9ba5d0230c0e38ca63c28c450b
rb5014 <5014@holbertonschool.com>
2022-06-13 14:52:49 +0200

    8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -11,6 +11,12 @@ void print_square(int size)
 {
 	int i;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
 	for (i = 1; i <= size * size; i++)
 	{
 		if (i % size == 0)
@@ -21,6 +27,4 @@ void print_square(int size)
 		else
 			_putchar('#');
 	}
-	if (size <= 0)
-		_putchar('\n');
 }
