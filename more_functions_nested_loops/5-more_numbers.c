bd8f2384d1aa52f5d713a326e433287f13cc2e0b
rb5014 <5014@holbertonschool.com>
2022-06-13 11:35:08 +0200

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times from 0 to 14, + \n
+ *
+ *
+ * Return: nothing(void)
+ */
+
+void more_numbers(void)
+{
+	int i, j, d1, d2;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			d1 = i / 10;
+			d2 = i % 10;
+			if (d1 != 0)
+			{
+				_putchar(d1);
+			}
+			_putchar(d2);
+		}
+		_putchar('\n');
+	}
+}
