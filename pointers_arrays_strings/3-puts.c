678a9690b31a527df14ae45485d24608b1b951dc
rb5014 <5014@holbertonschool.com>
2022-06-15 16:26:57 +0200

    3-puts.c : betty fixed

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts - prints a string, followed by a new line, to stdout
+ * _puts - prints a string, followed by a new line, to stdout
  *
  * @str: pointer containing string adress
  *
