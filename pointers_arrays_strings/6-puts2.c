c13d63b484c966a1ea40a4cebb479fcd38191b59
rb5014 <5014@holbertonschool.com>
2022-06-15 15:39:23 +0200

    0-reset_to_98.c + all files added + readme file

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - pointer as parameter,updates value it points to to 98
+ *
+ * @n: pointer to use as a parameter to pass a value to the var it points to
+ *
+ * Return: nothing(void)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
