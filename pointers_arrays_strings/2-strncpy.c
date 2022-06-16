852af1fd0a79edcd5e1b2fe72c0f96529b8dab07
rb5014 <5014@holbertonschool.com>
2022-06-16 15:48:54 +0200

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies up to n characters from a string to another
+ * Description : appends src string to dest string, overwriting null byte (\0)
+ * at the end of dest, then adds a teminating null byte
+ *
+ * @dest: pointer to destination string
+ * @src: pointer to a source string
+ * @n: number of bytes from src
+ *
+ * Return: pointer to the resulting string (dest)
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j = 0;
+
+	while (src[j])
+	{
+		dest[j] = src[j];
+			j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+	return (dest);
+}
