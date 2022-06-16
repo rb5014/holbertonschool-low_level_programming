d37668e2a4f7bc408351f9f71da0fba9a2baedb4
rb5014 <5014@holbertonschool.com>
2022-06-16 13:03:08 +0200

    9-strcpy.c betty fixed

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -3,8 +3,8 @@
 /**
  * _strcpy - copies the string pointed to src to the buffer pointed to by dest
  * + include \0
- * @n: pointer to use as a parameter to pass a value to the var it points to
- *
+ * @dest: pointer of a buffer that will receive a copy of a string
+ * @src: pointer of a string
  * Return: the pointer to dest
  */
 
@@ -12,11 +12,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = -1; /* start at -1 to get to 0 when enter loop */
 
-	do /* do while to do statements before condition checked */
-	{
+/* do while to do statements before condition checked */
+	do {
 		i++;
 		dest[i] = src[i];
-	}
-	while (src[i]);
-	return(dest);
+	} while (src[i]);
+	return (dest);
 }
