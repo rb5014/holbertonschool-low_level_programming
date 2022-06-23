0db827c9101e8cf3b1704e23ef24389354291b0d
rb5014 <5014@holbertonschool.com>
2022-06-23 21:11:17 +0200

    0-whatsmyname.c betty fixed

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -5,13 +5,13 @@
 /**
  * main - prints its name,followed by a new line
  *
- * @argc - number of arguments
- * @argv - array of strings containing the arguments
+ * @argc: number of arguments
+ * @argv: array of strings containing the arguments
  *
  * Return: Always 0 (Success)
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
