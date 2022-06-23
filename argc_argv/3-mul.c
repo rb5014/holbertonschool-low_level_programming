07c86381eac21cf7a6d78e5f8a237e76bdb982cd
rb5014 <5014@holbertonschool.com>
2022-06-23 21:39:54 +0200

    3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * print the result, + \n
+ * the two numbers and result of mul can be stored in an integer
+ * if no args, print 'Error' + \n and return 1
+ *
+ * @argc: number of arguments
+ * @argv: array of strings containing the arguments
+ *
+ * Return: 0 if success, or 1 if fail
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul = 0;
+
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+		return (mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
