e8ac388e45b16935f677f25437a3a59dac5a83ce
rb5014 <5014@holbertonschool.com>
2022-06-28 14:46:00 +0200

    1-strdup.c fixed: len =! 0 deleted

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -23,11 +23,11 @@ char *_strdup(char *str)
 
 	len = strlen(str);
 
-	cpy = malloc((len - 1) * sizeof(char));
+	cpy = malloc((len) * sizeof(char));
 
-	if (cpy && str && len != 0)
+	if (cpy && str)
 	{
-	for (i = 0; i < len && cpy; i++)
+	for (i = 0; i <= len; i++)
 		cpy[i] = str[i];
 	return (cpy);
 	}
