4ff9c8681bfb19a014e80b7d3c9d11830153307e
rb5014 <5014@holbertonschool.com>
2022-07-14 12:36:08 +0200

    0-print_list.c and lists.h fixed: NULL case added(return 0) + betty style for lists

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -4,14 +4,18 @@
  * print_list - prints all the elements of a list_t list
  * @h: pointer to list of type list_t
  * If str is NULL, print [0] (nil)
- * Return: number of nodes
+ * Return: number of nodes, 0 if null
  */
 
 size_t print_list(const list_t *h)
 {
 	size_t n = 1;
 
+	if (!(h))
+		return (0);
+
 	printf("[%u] %s\n", h->len, h->str == NULL ? "(nil)" : h->str);
+
 	if (h->next)
 		n += print_list(h->next);
 	return (n);
