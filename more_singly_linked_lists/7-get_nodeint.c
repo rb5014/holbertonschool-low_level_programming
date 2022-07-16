d11b779eb12f4cdfedc4ff66b6c8257a4f092517
rb5014 <5014@holbertonschool.com>
2022-07-16 15:06:35 +0200

    7-get_nodeint.c

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * get_notint_at_index - returns the nth node of a listint_t linked list.
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list.
  * @head: pointer to list of type listint_t
  * @index: index of the node, starting at 0
  * Return: nth node, 0 if null
