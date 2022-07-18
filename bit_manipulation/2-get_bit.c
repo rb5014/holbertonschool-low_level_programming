a44cdbcca40508987ae9df31e6029c2ea9dab8d8
rb5014 <5014@holbertonschool.com>
2022-07-18 12:58:56 +0200

    2-get_bit.c

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: unsigned long int to use
+ * @index: index, starting from 0, of the bit we return
+ * Return: value of the bit at index or -1 if an error occured
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int b = 0;
+
+	if (n > 1 && index > 0)
+		b = get_bit(n >> 1, index--) & 1;
+	else
+		b = n & 1;
+
+	return (b);
+}
