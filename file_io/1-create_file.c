afdce00311905d89263d9d1080b994f44a7ba05c
rb5014 <5014@holbertonschool.com>
2022-07-20 16:13:06 +0200

    1-create_file.c fixed: if text_content exists then write it else, empty file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -23,6 +23,7 @@ int create_file(const char *filename, char *text_content)
 	fd = creat(filename, 0600);
 	if (fd == -1)
 		return (-1);
-	write(fd, text_content, strlen(text_content));
+	if (text_content)
+		write(fd, text_content, strlen(text_content));
 	return (1);
 }
