0aaa5693713924627f2cd5c944fb0b296cfd16c3
rb5014 <5014@holbertonschool.com>
2022-07-20 16:07:24 +0200

    1-create_file.c

---
+++ file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Created file must have rw------- permissions. If the file already exists,
+ * do not change the permissions.
+ * If the file already exists, truncate it
+ * If text_content is NULL, create an empty file
+ *
+ * Return: 1 on succes, -1 on failure(file can not be created,
+ * file can not be written, write fails, etc)
+ * if filename is NULL, return -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!(filename))
+		return (-1);
+	fd = creat(filename, 0600);
+	if (fd == -1)
+		return (-1);
+	write(fd, text_content, strlen(text_content) + 1);
+	return (1);
+}
