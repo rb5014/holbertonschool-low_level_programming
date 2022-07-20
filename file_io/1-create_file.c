22f4c5e6e2124e7b22aac649f11675a87dcb68fd
rb5014 <5014@holbertonschool.com>
2022-07-20 16:09:00 +0200

    1-create_file.c fixed strlen in write

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -23,6 +23,6 @@ int create_file(const char *filename, char *text_content)
 	fd = creat(filename, 0600);
 	if (fd == -1)
 		return (-1);
-	write(fd, text_content, strlen(text_content) + 1);
+	write(fd, text_content, strlen(text_content));
 	return (1);
 }
