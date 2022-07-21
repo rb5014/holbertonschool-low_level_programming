2fdae8901f69e812590029dd606d1e7fb3382424
rb5014 <5014@holbertonschool.com>
2022-07-21 18:05:02 +0200

    3-cp.c

---
+++ file_io/3-cp.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - Entry point, copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: array of strings(arguments)
+ * If the number of args is not the correct one, exit(97) and print
+ * Usage: cp file_from file_to, +\n to the standar error
+ * If file_to already exists, truncate it
+ * If file_from does not exist, or if it can't be red, exit(98) and print
+ * Error: Can't read from file NAME_OF_THE_FILE, +\n, on STDERR_FILENO
+ * if you can not create or if write to file_to fails, exit with code 99 and
+ * print Error: Can't write to NAME_OF_THE_FILE, followed by a new line, on the
+ * POSIX standard error
+ * if you can not close a file descriptor , exit with code 100 and print
+ * "Error: Can't close fd FD_VALUE", followed by a new line,
+ * on the standard error
+ * Permissions of the created file: rw-rw-r--. If the file already exists,
+ * do not change the permissions
+ * You must read 1,024 bytes at a time from the file_from to make less
+ * system calls. Use a buffer
+ * You are allowed to use dprintf
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	char buffer[1024];
+	int fd, wr, cl, rd;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	rd = read(fd, buffer, 1024);
+	cl = close(fd);
+	fd = creat(argv[2], 0664);
+	wr = write(fd, buffer, rd);
+	if (fd == -1 | wr == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	cl = close(fd);
+	if (cl == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	return (0);
+}
