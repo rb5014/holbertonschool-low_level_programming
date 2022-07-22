cefa5374b76a57727cd58835be068acee8440896
rb5014 <5014@holbertonschool.com>
2022-07-22 19:19:24 +0200

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,19 +11,22 @@
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
-{	
+{
 	if (h)
 	{
-		
+		dlistint_t *new;
+
+		new = malloc(sizeof(dlistint_t));
+		if (!(new))
+			return (NULL);
+
 		if (idx == 0)
-			return (add_dnodeint(h, n));
+			new = add_dnodeint(h, n);
 		else
 		{
-			dlistint_t *new, *tmp = *h;
+			dlistint_t *tmp = *h;
 			unsigned int i = 0;
-			new = malloc(sizeof(dlistint_t));
-			if (!(new))
-				return (NULL);
+
 			while (i < idx - 1 && tmp)
 			{
 				tmp = tmp->next;
@@ -37,10 +40,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new->prev = tmp;
 			if (new->next)
 				new->next->prev = new;
-			
-			return (new);
+
 		}
+		return (new);
 	}
 	return (NULL);
-	
+
 }
