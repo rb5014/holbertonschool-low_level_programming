e5f91f00bb3fd2f61fcb29adba9d22bc79d540f9
rb5014 <5014@holbertonschool.com>
2022-07-22 20:00:33 +0200

    8-delete_dnodeint.c fixed: syntax

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -18,7 +18,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			tmp = tmp->next;
 			index--;
 		}
-		if (!(tmp) | index > 0)
+		if ((!(tmp)) | (index > 0))
 			return (-1);
 		if (tmp == *head)
 			*head = tmp->next;
