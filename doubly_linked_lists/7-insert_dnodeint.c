12af32e8a4ff4fac4d0bbf8b48ce742e005434c3
rb5014 <5014@holbertonschool.com>
2022-07-22 18:44:04 +0200

    7-insert_dnodeint.c fixed

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -36,6 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (i == idx - 1)
 		{
 			new->next = tmp->next;
+			new->prev = tmp;
 			tmp->next = new;
 		}
 		return (new);
