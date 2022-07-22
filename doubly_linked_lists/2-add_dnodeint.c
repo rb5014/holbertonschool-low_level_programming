378ec930425b0b457a77f408b7c1e45eb7e02ee9
rb5014 <5014@holbertonschool.com>
2022-07-22 13:12:45 +0200

    2-add_dnodeint.c fixed syntax

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -6,9 +6,9 @@
  * Return: the adress of the new element, or NULL if it failed
  */
 
-dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	listint_t *new;
+	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
