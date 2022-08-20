f62911239d49bda8f1d652b36e322c114e7fd8d2
rb5014 <5014@holbertonschool.com>
2022-08-20 19:24:30 +0200

    3-hash_table_set.c fixed

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,27 +1,52 @@
 #include "hash_tables.h"
+
+/**
+ * add_first - create and add a node at the beginning of a linked list
+ *
+ * @head: head of a linked list
+ * @key: the key
+ * @value: the vqlue qssociqted to the key
+ */
+void add_first(hash_node_t **head, const char *key, const char *value)
+{
+	hash_node_t *new = malloc(sizeof(hash_node_t));
+
+	if (!(new))
+		return;
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = *head;
+	*head = new;
+}
 /**
  * hash_table_set - adds an element to the hash table
  * @ht:  the hash table you want to add or update the key/value to
  * @key: the key. key can not be an empty string
- * @value: the value associated with the key. value must be duplicated. 
- * value can be an empty string 
+ * @value: the value associated with the key. value must be duplicated.
+ * value can be an empty string
  * In case of collision, add the new node at the beginning of the list
  * Return: 1 if it succeeded, 0 otherwise
  */
- int hash_table_set(hash_table_t *ht, const char *key, const char *value)
- {
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
 	unsigned long int index;
-	hash_node_t *hn = malloc(sizeof(hash_node_t));
+	hash_node_t *tmp;
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
-	    key == NULL || strlen(key) == 0 || value == NULL)
+		key == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
-	if (ht->array[index] && strcmp(ht->array[index]->key, key) != 0)
-		hn->next = NULL;
-	hn->key = strdup(key);
-	hn->value = strdup(value);
-	ht->array[index] = hn;
-	return(1);
- }
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			tmp->value = strdup(value);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	add_first(&(ht->array[index]), key, value);
+	return (1);
+}
